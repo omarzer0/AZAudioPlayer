08b88ef0d2d2091e4eecc5252f60008bb0c7795d
omarzer0 <omarzero1015@gmail.com>
2022-10-07 22:01:25 +0200

    Add Scan local fragment

---
+++ app/src/main/java/az/zero/azaudioplayer/ui/screens/scan_local_files/ScanLocalFragment.kt
@@ -0,0 +1,20 @@
+package az.zero.azaudioplayer.ui.screens.scan_local_files
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import az.zero.azaudioplayer.core.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ScanLocalFragment : BaseFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        return setFragmentContent {
+
+        }
+    }
+}
---
+++ app/src/main/java/az/zero/azaudioplayer/ui/screens/scan_local_files/ScanLocalViewModel.kt
@@ -0,0 +1,13 @@
+package az.zero.azaudioplayer.ui.screens.scan_local_files
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ScanLocalViewModel @Inject constructor(
+
+) : ViewModel() {
+
+}
+
